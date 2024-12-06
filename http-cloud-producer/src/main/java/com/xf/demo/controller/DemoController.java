d47738062626c45e64c655904605b672f2c26c89
xiongf <2439534736@qq.com>
2024-12-06 17:13:04 +0800

    集成消息队列stream rocketmq，并实现消费者监听单个队列消息并消费

--- http-cloud-producer/src/main/java/com/xf/demo/controller/DemoController.java
+++ http-cloud-producer/src/main/java/com/xf/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.xf.demo.controller;
 
 import com.xf.demo.mapper.SeataProductMapper;
+import com.xf.demo.mq.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,9 @@ public class DemoController {
     @Autowired
     private SeataProductMapper seataProductMapper;
 
+    @Autowired
+    private MessageProducer messageProducer;
+
     @GetMapping(value = "/test/feign/{string}")
     public String test(@PathVariable String string) {
         return string;
@@ -33,5 +37,27 @@ public class DemoController {
     @GetMapping(value = "/test/seata/deInventory")
     void deInventorySeata(@RequestParam("num") Integer num, @RequestParam("productId") Integer productId) {
         seataProductMapper.deInventory(num, productId);
+    }
+
+    /**
+     * 发送消息队列 topic0
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/test/mq/topic0")
+    String sendMqTopic0(@RequestParam("message") String message) {
+        messageProducer.sendMessageToOutput0(message);
+        return "topic0消息发送成功了~~~";
+    }
+
+    /**
+     * 发送消息队列 topic1
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/test/mq/topic1")
+    String sendMqTopic1(@RequestParam("message") String message) {
+        messageProducer.sendMessageToOutput1(message);
+        return "topic1消息发送成功了~~~";
     }
 }
