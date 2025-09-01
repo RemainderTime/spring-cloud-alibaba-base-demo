05528bf5e27f0263bc9a5e21ec981ca7ea42033d
海言 <2439534736@qq.com>
2025-09-01 18:23:47 +0800

    调整网关路由配置

--- cloud-producer/src/main/java/com/xf/demo/controller/DemoController.java
+++ cloud-producer/src/main/java/com/xf/demo/controller/DemoController.java
@@ -3,10 +3,7 @@ package com.xf.demo.controller;
 import com.xf.demo.mapper.SeataProductMapper;
 import com.xf.demo.mq.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * packageName com.xf.demo.controller
